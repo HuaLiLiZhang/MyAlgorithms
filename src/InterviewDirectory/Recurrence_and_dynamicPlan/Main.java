6cb2821547ef2a80ba6faac5d31ebbd599107992
HuaLiLiZhang <1151874043@qq.com>
2018-03-19 20:58:59 +0800

    换钱的最小货币数

--- src/InterviewDirectory/Recurrence_and_dynamicPlan/Main.java
+++ src/InterviewDirectory/Recurrence_and_dynamicPlan/Main.java
@@ -6,12 +6,19 @@ package InterviewDirectory.Recurrence_and_dynamicPlan;
 public class Main {
     public static void main(String []args)
     {
-        int[][]m = {{1,3,5,9},{8,1,3,4},{5,0,6,1},{8,8,4,0}};
-        int minpath = new minPathSum1().minpathsum(m);
-        System.out.println(minpath);
-        int minpath1 = new minPathSum1().minPathArr(m);
-        System.out.println(minpath1);
-        int min3 = new minPathSum1().minpath(m);
-        System.out.println(min3);
+        //int[][]m = {{1,3,5,9},{8,1,3,4},{5,0,6,1},{8,8,4,0}};
+        //int minpath = new minPathSum1().minpathsum(m);
+        //System.out.println(minpath);
+        //int minpath1 = new minPathSum1().minPathArr(m);
+        //System.out.println(minpath1);
+        //int min3 = new minPathSum1().minpath(m);
+        //System.out.println(min3);
+
+        int[]arr={1,4,2,3};
+        mincoins1 mincoins1 =  new mincoins1();
+        int num = mincoins1.minCoins1(arr,20);
+        System.out.println(num);
+        int num2 = mincoins1.minCoins2(arr,20);
+        System.out.println(num2);
     }
 }
---
+++ src/InterviewDirectory/Recurrence_and_dynamicPlan/mincoins1.java
@@ -0,0 +1,65 @@
+package InterviewDirectory.Recurrence_and_dynamicPlan;
+
+/**
+ * Created by huali on 2018/3/19.
+ */
+public class mincoins1 {
+    //时间复杂度为O(Nxaim) ,空间复杂度为：O(Nxaim)
+    public int minCoins1(int []arr,int aim)
+    {
+        if(arr==null||arr.length==0||aim<0)
+            return -1;
+        int n = arr.length;
+        int max = Integer.MAX_VALUE;
+        int [][]dp = new int [n][aim+1];
+        for(int j=1;j<=aim;++j)
+        {
+            dp[0][j]=max;
+            if(j-arr[0]>=0&&dp[0][j-arr[0]]!=max)
+                dp[0][j]=dp[0][j-arr[0]]+1;
+        }
+
+        int left = 0;
+        for(int i=1;i<n;++i)
+        {
+            for(int j=1;j<=aim;++j)
+            {
+                left = max;
+                if(j-arr[i]>=0&&dp[i][j-arr[i]]!=max)
+                    left = dp[i][j-arr[i]]+1;
+                dp[i][j] = Math.min(left,dp[i-1][j]);
+            }
+        }
+        return dp[n-1][aim]!=max?dp[n-1][aim]:-1;
+    }
+
+    //时间复杂度为O(Nxaim) ,空间复杂度为：O(aim) ,一维数组
+    public int minCoins2(int[]arr,int aim)
+    {
+        if(arr==null||arr.length==0||aim<0)
+            return -1;
+        int n=arr.length;
+        int max = Integer.MAX_VALUE;
+        int []dp = new int[aim+1];
+        for(int j=1;j<=aim;++j)
+        {
+            dp[j]=max;
+            if(j-arr[0]>=0&&dp[j-arr[0]]!=max)
+                dp[j]=dp[j-arr[0]]+1;
+        }
+        int left = 0;
+        for(int i=1;i<n;++i)
+        {
+            for(int j=1;j<=aim;j++)
+            {
+                left = max;
+                if(j-arr[i]>=0&&dp[j-arr[i]]!=max)
+                {
+                    left = dp[j-arr[i]]+1;
+                }
+                dp[j]=Math.min(left,dp[j]);
+            }
+        }
+        return dp[aim]!=max?dp[aim]:-1;
+    }
+}
