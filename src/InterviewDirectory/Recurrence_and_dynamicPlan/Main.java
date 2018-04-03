dc1c5045d10e2c7ade72c4020576f4c8deb7b7e6
HuaLiLiZhang <1151874043@qq.com>
2018-04-03 16:34:12 +0800

    数组最长连续子序列

--- src/InterviewDirectory/Recurrence_and_dynamicPlan/Main.java
+++ src/InterviewDirectory/Recurrence_and_dynamicPlan/Main.java
@@ -103,17 +103,17 @@ public class Main {
         //int num = new jumpNumLest().jump1(arr);
         //System.out.println(num);
 
-        //int []arr = {100,4, 200, 2,3,1};
-        //longestConsecutive longest = new longestConsecutive();
-        //int result = longest.longestConse(arr);
-        //System.out.println(result);
+        int []arr = {100,4, 200, 2,1,3};
+        longestConsecutive longest = new longestConsecutive();
+        int result = longest.longestConse(arr);
+        System.out.println(result);
 
-        Nqueue queuq = new Nqueue();
-        int num1 = queuq.num1(4);
-        System.out.println(num1);
-
-        int num2 = queuq.num2(4);
-        System.out.println(num2);
+        //Nqueue queuq = new Nqueue();
+        //int num1 = queuq.num1(4);
+        //System.out.println(num1);
+        //
+        //int num2 = queuq.num2(4);
+        //System.out.println(num2);
 
     }
 }
--- src/InterviewDirectory/Recurrence_and_dynamicPlan/longestConsecutive.java
+++ src/InterviewDirectory/Recurrence_and_dynamicPlan/longestConsecutive.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 /**
  * Created by huali on 2018/4/2.
  */
-public class longestConsecutive {
+public class longestConsecutive {//数组的最长子序列{100,4,2333,3,2,1}；最长子序列为：1,2,3,4
     public int longestConse(int[] arr)
     {
         if(arr==null||arr.length==0)
@@ -17,14 +17,16 @@ public class longestConsecutive {
             if(!map.containsKey(arr[i]))
             {
                 map.put(arr[i], 1);
+                if(map.containsKey(arr[i]-1))
+                {
+                    max = Math.max(max, merge(map, arr[i]-1, arr[i]));
+                }
+
                 if(map.containsKey(arr[i]+1))
                 {
                     max = Math.max(max, merge(map, arr[i],arr[i]+1));
                 }
-                if(map.containsKey(arr[i]-1))
-                {
-                    max = Math.max(max, merge(map, arr[i]-1,arr[i]));
-                }
+
             }
         }
         return max;
@@ -32,6 +34,9 @@ public class longestConsecutive {
 
     public int merge(HashMap<Integer, Integer> map , int less , int more)
     {
+        //less = less - map.get(less)+1,这里是序列最小值可以到几，就是说3 的话前面有两个，就是从1到3.
+        //所以left 是1， right是3，
+        //同理如果是比他大，那么最大值扩展。
         int left = less - map.get(less) +1;
         int right = more + map.get(more)-1;
         int len = right - left+1;
