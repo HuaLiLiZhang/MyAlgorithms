54b0b2a837d468a1b727f83a45cc8ab0789dcbf5
HuaLiLiZhang <1151874043@qq.com>
2018-04-07 21:56:22 +0800

    自动装箱的陷阱

--- src/InterviewDirectory/StringProblem/Main.java
+++ src/InterviewDirectory/StringProblem/Main.java
@@ -39,7 +39,23 @@ public class Main {
         //String str = "()(()()(";
         //System.out.println(new MaxLength().maxlength(str));
 
-        String str = "48*((70-65)-43)+8*1";
-        System.out.println(new str_getValue().getValue(str));
+        //String str = "48*((70-65)-43)+8*1";
+        //System.out.println(new str_getValue().getValue(str));
+
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c==d);  //true
+        System.out.println(e==f);  //false
+        System.out.println(c==(a+b));  //true
+        System.out.println(c.equals(a+b));  //true
+        System.out.println(g==(a+b));  //true
+        System.out.println(g.equals(a+b));  //false
+
+
     }
 }
