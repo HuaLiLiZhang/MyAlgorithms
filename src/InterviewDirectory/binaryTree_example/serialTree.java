8b6f37ff245f246ffe1a91a0d86211482621bc97
HuaLiLiZhang <1151874043@qq.com>
2018-04-12 13:47:41 +0800

    二叉树层级遍历序列化和反序列化

--- src/InterviewDirectory/binaryTree_example/serialTree.java
+++ src/InterviewDirectory/binaryTree_example/serialTree.java
@@ -40,4 +40,65 @@ public class serialTree {
         return head;
     }
 
+
+    //层序遍历的序列化和反序列化
+    public String serialBylevel(TreeNode head) {
+        if (head == null)
+            return "#!";
+        String res = head.val + "!";
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(head);
+        while (!queue.isEmpty())
+        {
+            head = queue.poll();
+            if (head.left != null) {
+                res += head.left.val + "!";
+                queue.offer(head.left);
+            } else {
+                res += "#!";
+            }
+            if (head.right != null) {
+                res += head.right.val + "!";
+                queue.offer(head.right);
+            } else {
+                res += "#!";
+            }
+        }
+        return res;
+    }
+
+    //反序列化
+    public TreeNode reconBylevelString(String levelStr)
+    {
+        String [] values = levelStr.split("!");
+        int index = 0;
+        TreeNode head = generateNodeByString(values[index++]);
+        Queue<TreeNode> queue = new LinkedList<>();  //队列先进先出，
+        if(head!=null)
+            queue.offer(head);
+        TreeNode node = null;
+        while (!queue.isEmpty())
+        {
+            node = queue.poll();
+            node.left = generateNodeByString(values[index++]);
+            node.right = generateNodeByString(values[index++]);
+            if(node.left!=null)
+            {
+                queue.offer(node.left);
+                //left先进来，left先出去，在遍历left的子节点。
+            }
+            if(node.right!=null)
+            {
+                queue.offer(node.right);
+            }
+        }
+        return head;
+    }
+    public TreeNode generateNodeByString(String value)
+    {
+        if(value.equals("#"))
+            return null;
+        return new TreeNode(Integer.valueOf(value));
+
+    }
 }
