945c2dfdc99f47a0413df384dd0b54ac0e985dab
HuaLiLiZhang <1151874043@qq.com>
2018-04-14 22:02:54 +0800

    大顶堆小顶堆数据随机删除？

---
+++ src/InterviewDirectory/Alibaba/deleteElement.java
@@ -0,0 +1,20 @@
+package InterviewDirectory.Alibaba;
+
+/**
+ * Created by huali on 2018/4/14.
+ */
+public class deleteElement {
+    //deleteElement(int[] heap, int index, int len) {
+    //    mutex.lock();
+    //    heap[index] = heap[len-1];
+    //    len--;
+    //    while(2*index + 1 < len && (heap[index] > heap[2*index+1]|| heap[index] > heap[2*index +2])) {
+    //        int swapi = (heap[2*index+1] < heap[2*index +2])? 2*index+1:2*index+2;
+    //        swap(heap,index,swapi);
+    //        index = swapi;
+    //    }
+    //    mutex.unlock();
+    //}
+
+    
+}
