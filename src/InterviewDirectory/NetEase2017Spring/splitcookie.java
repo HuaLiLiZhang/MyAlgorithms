8d7ecb8356c9c833856b8dd5e4c8bc9524d899f0
HuaLiLiZhang <1151874043@qq.com>
2018-05-08 13:59:52 +0800

    输出之前的队列样子

--- src/InterviewDirectory/NetEase2017Spring/splitcookie.java
+++ src/InterviewDirectory/NetEase2017Spring/splitcookie.java
@@ -66,14 +66,24 @@ public class splitcookie {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
         int n = sc.nextInt();
-
+// dp[i][j]表示前i位（高位开始）余数为j的个数
         long[][]dp = new long[s.length()+1][n];
         dp[0][0]=1;
+
+
+////前i-1字符串组成的整数 除n得的余数为j的情况有dp[i - 1][j]种，余数只能在0到n-1的范围
+////假设前i-1字符串组成的数是125，n=4，余数为125 % 4 = 1，
+////所以dp[3][0]=0,dp[3][1]=1,dp[3][2]=0,dp[3][3] = 0
+////假设第i个字符为6，所以新的数变成1256，1256 % n就相当于(125 * 10 + 6) % n
+////相当于[(124 + 1)*10 + 6] % n,因为前i-1个组成的数中125 % n = 1，所以(125 - 1) * 10是能除尽n的
+////所以就相当于[(1 * 10) + 6] % n ,而 1 就是前i-1个组成的数除n得到的余数，所以前i-1个组成的数除n有多少个余
+////数为1的情况，那么前i个组成的数就有多少种余数为[(1*10) + 6] % n 的情况。
+////for(int j = 0; j < n; j++) 此for循环中的j代表前i-1个组成的数除n得到的余数，余数从0到n-1
         for(int i =1;i<=s.length();i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(s.charAt(i-1)=='X')
+                if(s.charAt(i-1)=='X')  // 如果当前位为X，用0-9带入
                 {
                     for(int k =0;k<=9;k++)
                     {
@@ -82,7 +92,7 @@ public class splitcookie {
                     }
                 }
                 else
-                {
+                { // 如果当前位上的数字为不为X，用具体值带入
                     int newJ = (j*10+s.charAt(i-1)-'0')%n;
                     dp[i][newJ] +=dp[i-1][j];
                 }
