fb1964cf0b06d7340908ba0346b4d78a3a485270
HuaLiLiZhang <1151874043@qq.com>
2018-05-22 22:41:30 +0800

    喜欢的字符串，不包含小写，不包含连续相同的字母，不包含类似ABACADA 有AAA，或者THZSATHA包含TH。。。。

--- src/InterviewDirectory/NetEase/likeword.java
+++ src/InterviewDirectory/NetEase/likeword.java
@@ -29,35 +29,115 @@ public class likeword {
 //输出例子1:
 //Dislikes
 
+
+    //使用正则表达式，简则666的不行
+
     public static void main(String []args)
     {
         Scanner sr = new Scanner(System.in);
         while (sr.hasNext())
         {
-            String  str = sr.nextLine();
-            char [] ch = str.toCharArray();
-            int i=0;
-            for(;i<ch.length;i++)
-            {
-                if(ch[i]>'Z'&&ch[i]<'A')
-                {
-                    System.out.println("Dislikes");
-                    break;
-                } else  if(i<ch.length-1&&ch[i]==ch[i+1])
-                {
-                    System.out.println("Dislikes");
-                    break;
-                }
-
-            }
-            if(i==ch.length)
+            String  word = sr.nextLine();
+            if(isAllUpCase(word) && isConEql(word) && isThrEql(word))
             {
                 System.out.println("Likes");
             }
-
+            else {
+                System.out.println("Dislikes");
+            }
 
         }
         sr.close();
 
     }
+
+    private static boolean isConEql(String word) {
+        //2.单词没有连续相等的字母
+        return !word.matches(".*(.)(\\1).*");
+    }
+
+    private static boolean isThrEql(String word) {
+//3.单词没有形如“xyxy”(这里的x，y指的都是字母，并且可以相同)这样的子序列，子序列可能不连续。
+        return !word.matches(".*(.).*(.)(.*\\1)(.*\\2).*");
+    }
+
+    private static boolean isAllUpCase(String word) {
+        //单词每个字母都是大写字母
+        return word.matches("[A-Z]+");
+    }
+
+
+    //题目测试用例的漏洞
+    //public static void main(String []args)
+    //{
+    //    Scanner sr = new Scanner(System.in);
+    //    while (sr.hasNext())
+    //    {
+    //        String  str = sr.nextLine();
+    //        char [] ch = str.toCharArray();
+    //        int i=0;
+    //        for(;i<ch.length;i++)
+    //        {
+    //            if(ch[i]>'Z'&&ch[i]<'A')
+    //            {
+    //                System.out.println("Dislikes");
+    //                break;
+    //            } else  if(i<ch.length-1&&ch[i]==ch[i+1])
+    //            {
+    //                System.out.println("Dislikes");
+    //                break;
+    //            }
+    //
+    //        }
+    //        if(i==ch.length)
+    //        {
+    //            System.out.println("Likes");
+    //        }
+    //
+    //
+    //    }
+    //    sr.close();
+    //
+    //}
+
+
+//真正的解答。
+//import java.util.*;
+// 
+//public class Main{
+//    public static boolean judge(String s){
+//        char [] chars = s.toCharArray();
+//        char tmp = 0;
+//        for (char c : chars){
+//            if (c >'Z' || c < 'A') return false;
+//            if (tmp == c) return false;
+//            tmp = c;
+//        }
+//        Set<Character> charSet = new HashSet<>();
+//        int count;
+//        for (int i = 0; i < chars.length; i++){
+//            charSet.clear();
+//            count = 1;
+//            for (int j = i + 1; j < chars.length; j++){
+//                if (chars[i] == chars [j])
+//                {
+//                    count++;
+//                    if (count > 3)
+//                          return false;
+//                }else if (count > 1 && charSet.contains(chars[j])) return false;
+//                else if (count == 1) charSet.add(chars[j]);
+// 
+//            }
+//        }
+//        return true;
+//    }
+//    public static void  main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        while(sc.hasNext()){
+//            String s = sc.nextLine();
+//            System.out.println(judge(s)?"Likes":"Dislikes");
+//        }
+//        sc.close();
+//    }
+//}
 }
