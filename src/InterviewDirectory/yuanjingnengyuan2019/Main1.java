c4e856f8502103532b4cdcca9f1a70f3e6b5dc41
HuaLiLiZhang <1151874043@qq.com>
2018-08-27 18:31:46 +0800

    实现下n个排列，重新排列数字，使它们按字典序下一大的排列。

--- src/InterviewDirectory/yuanjingnengyuan2019/Main1.java
+++ src/InterviewDirectory/yuanjingnengyuan2019/Main1.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class Main1 {
     //LeetCode上面的题，
-    //实现下一个排列，重新排列数字，使它们按字典序下一大的排列。
+    //实现下n个排列，重新排列数字，使它们按字典序下一大的排列。
     //
     //如果没有下一大的排列，那么就排成字典序最小的排列。
     //
@@ -24,6 +24,10 @@ public class Main1 {
     //10 2
     //1 2 3 4 5 6 7 8 9 10
     //1 2 3 4 5 6 7 9 8 10
+
+    //9 3
+    //2 3 4 1 5 6 7 9 8
+    //2 3 4 1 5 6 9 7 8
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext())
@@ -39,6 +43,10 @@ public class Main1 {
             for(int i = 0;i<k;i++)
             {
                 nextPermutation(num);
+            //    初始排列      2 3 4 1 5 6 7 9 8
+            //    下第一个排列： 2 3 4 1 5 6 8 7 9
+            //    下第二个排列   2 3 4 1 5 6 8 9 7
+            //    下第三个排列   2 3 4 1 5 6 9 7 8
             }
             for(int i : num)
             {
@@ -55,15 +63,21 @@ public class Main1 {
             return ;
         int i=num.length-2;
         //从后往前看最大升序的位置。
+        //9 3
+        //2 3 4 1 5 6 7 9 8
+        //最大位置就是 10所在位置， i = 6
         while(i>=0&&num[i]>=num[i+1])
             i--;
         if(i>=0){
             int j=i+1;
+            //从下一个位置开始，找到后面降序排列中较小的元素，但依然比i元素大的位置，然后交换
             while(j<num.length&&num[j]>num[i])
                 j++;
             j--;
             swap(num,i,j);
         }
+        //然后在从i+1开始到arr.length-1，逆序排列剩下元素。
+        //则是下一个较大的元素排列。
         reverse(num,i+1,num.length-1);
     }
     public static void swap(int[] num,int i,int j){
