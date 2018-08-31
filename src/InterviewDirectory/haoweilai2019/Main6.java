e948334bd58940a35209d69d3754a04ff3a6c974
HuaLiLiZhang <1151874043@qq.com>
2018-08-31 12:02:05 +0800

    替换String s中的子串s1为s2。

--- src/InterviewDirectory/haoweilai2019/Main6.java
+++
@@ -1,7 +0,0 @@
-package InterviewDirectory.haoweilai2019;
-
-/**
- * Created by huali on 2018/8/28.
- */
-public class Main6 {
-}
---
+++ src/InterviewDirectory/haoweilai2019/replacesubString.java
@@ -0,0 +1,23 @@
+package InterviewDirectory.haoweilai2019;
+
+import java.util.Scanner;
+
+/**
+ * Created by huali on 2018/8/28.
+ */
+public class replacesubString {
+    public static void main(String[] args)
+    {
+
+        Scanner in = new Scanner(System.in);
+        String s1 = in.nextLine();
+        String s2 = in.nextLine();
+        String s3 = in.nextLine();
+        System.out.println(replacesubstr(s1,s2,s3));
+
+        }
+        public static String replacesubstr(String s1,String s2,String s3){
+            return s1.replaceAll(s2,s3);
+        }
+
+}
