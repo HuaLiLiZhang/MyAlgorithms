9730ac8690c899e749c6761c1f817f3175507160
HuaLiLiZhang <1151874043@qq.com>
2018-08-31 11:07:33 +0800

    如果把棋盘竖起来，小木块会向下掉落，直至落到障碍物上，或者落到障碍物上叠加的小木块，如果底部没有障碍物，则掉出棋盘

--- src/InterviewDirectory/pingduoduo2019_2/chessBoardooxx.java
+++ src/InterviewDirectory/pingduoduo2019_2/chessBoardooxx.java
@@ -5,7 +5,34 @@ import java.util.Scanner;
 /**
  * Created by huali on 2018/8/30.
  */
-public class Main2 {
+public class chessBoardooxx {
+    //长方形的棋盘，每个格子可能是一下三种情况：
+    //A放障碍物，不可移动，B放小木块，C格子为空。
+    //如果把棋盘竖起来，小木块会向下掉落，直至落到障碍物上，或者落到障碍物上叠加的小木块，如果底部没有障碍物，则掉出棋盘
+
+    //问：最后棋盘的 状态。
+
+//测试用例:
+//3 4
+//.oxo
+//o..o
+//.xox
+//
+//结果
+//..xo
+//.o.o
+//.x.x
+
+//4 5
+//ox.ox
+//.ooxo
+//..xoo
+//oxo..
+//.x.ox
+//..ox.
+//.ox..
+//.x...
+
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
