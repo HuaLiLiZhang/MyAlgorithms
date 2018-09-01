89ae0b88a551c178c9676db0ef25091e39d9ffee
HuaLiLiZhang <1151874043@qq.com>
2018-09-01 21:07:34 +0800

    头条二次笔试，一个团队有10个人，每一行代表这个人认识那个人，如果相互认识，则在一组，或者a->c，c->b，那么a->b，abc也很快认识，也在一组->并查集权重法

--- src/InterviewDirectory/toutiao2019_2/isSameSetUnionSet.java
+++ src/InterviewDirectory/toutiao2019_2/isSameSetUnionSet.java
@@ -6,7 +6,7 @@ import java.util.*;
  * Created by huali on 2018/8/25.
  */
 public class isSameSetUnionSet {
-//    一个团队有10个人，每一行代表这个人认识那个人，如果相互认识，或者a->c，c->b，那么a->b，
+//    一个团队有10个人，每一行代表这个人认识那个人，如果相互认识，则在一组，或者a->c，c->b，那么a->b，abc也很快认识，也在一组
 //10
 //0
 //5 3 0
@@ -117,65 +117,71 @@ public class isSameSetUnionSet {
     }
 }
 
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//class peoplenum {
-//    vector<int> parent;
-//    vector<int> rank;
-//    vector<int> sz;
-//    public:
-//    peoplenum(int N) : parent(N), rank(N), sz(N, 1) {
-//        for (int i = 0; i < N; ++i)
-//            parent[i] = i;
-//    }
+class WeightedQuickUnion {  //思路与此题一样。
+//    加权快速合并(Weighted quick-union)
+//数据结构
+//与快速合并类似，但是维护了一个额外的数组sz[i],它包含了以i为根节点的树的节点数量。
 //
-//    int find(int x) {
-//        if (parent[x] != x) parent[x] = find(parent[x]);
-//        return parent[x];
-//    }
+//查找
+//与快速合并完全相同
 //
-//    void Union(int x, int y) {
-//        int xr = find(x), yr = find(y);
-//        if (xr == yr) return;
-//
-//        if (rank[xr] < rank[yr]) {
-//            int tmp = yr;
-//            yr = xr;
-//            xr = tmp;
-//        }
-//        if (rank[xr] == rank[yr])
-//            rank[xr]++;
-//
-//        parent[yr] = xr;
-//        sz[xr] += sz[yr];
-//    }
-//
-//    int size(int x) {
-//        return sz[find(x)];
-//    }
-//
-//    int top() {
-//        return size(sz.size() - 1) - 1;
-//    }
-//};
-//
-//int main()
-//        {
-//        int n; cin >> n;
-//        peoplenum d(n + 1);
-//        for (int i = 0; i < n; ++i)
-//        {
-//        int v;
-//        while (cin >> v && v)
-//        {
-//        d.Union(i + 1, v);
-//        }
-//        }
-//        int ans = 0;
-//        for (int i = 1; i <= n; ++i)
-//        {
-//        if (d.find(i) == i) ans++;
-//        }
-//        cout << ans << endl;
-//        }
+//合并
+//将小树指向大树的根节点
+//更新sz[]数组
+
+    private static int [] id ;
+    private static int[] sz;//size of compoent for roots
+    private static int count;//number of components
+
+    //public static void main(String[] args)
+    //{
+    //    Scanner sc = new Scanner(System.in);
+    //    int n = sc.nextInt(); //14
+    //    id = new int[n];
+    //    sz = new int[n];
+    //    for(int i = 0;i<n;i++)
+    //    {
+    //        id[i] = i;
+    //        sz[i] = 1;
+    //    }
+    //    count = n;
+    //    //14
+    //    //7
+    //    int [] inputs = {0,1,9,4,9,6,6,7,8,9,10,12,10,11};
+    //    //int [] inputs = {0,1,9,4,9,6,6,7,8,9};
+    //    for(int i = 0;i<n;i++)
+    //    {
+    //        if(!isSameset(i,inputs[i]))
+    //        {
+    //            Union(i,inputs[i]);
+    //            count--;
+    //        }
+    //    }
+    //    System.out.println(count);
+    //}
+
+    private static void Union(int p, int q) {
+        int pvalue = root(p);
+        int qvalue = root(q);
+        if(pvalue==qvalue)
+            return ;
+        if(sz[pvalue]<sz[qvalue])
+        {
+            id[pvalue] = qvalue;
+            sz[qvalue]+=sz[pvalue];
+        }else {
+            id[qvalue] = pvalue;
+            sz[pvalue] +=sz[qvalue];
+        }
+    }
+
+    private static boolean isSameset(int p, int q) {
+        return root(p)==root(q);
+    }
+
+    private static int root(int p) {
+        while (p!=id[p])
+            p = id[p];//p和id[p] 一起更新
+        return p;
+    }
+}
