62f4fec216c8650504d4491544524d25049ae56b
HuaLiLiZhang <1151874043@qq.com>
2018-09-01 22:02:28 +0800

    双生词（假如两个字符串分别为S和S'）若存在双生词则输出：Yeah，否则输出Sad

--- src/InterviewDirectory/toutiao2019_2/ShuangShengCiTwoStr_randomCut_reverse.java
+++ src/InterviewDirectory/toutiao2019_2/ShuangShengCiTwoStr_randomCut_reverse.java
@@ -7,9 +7,27 @@ import java.util.Scanner;
 /**
  * Created by huali on 2018/8/25.
  */
-public class num3 {
-    //
+public class ShuangShengCiTwoStr_randomCut_reverse {
+    //题目描述：双生词满足如下条件的两个字符串：（假如两个字符串分别为S和S'）
+//    1.字符串S和S'长度相同
+//    2.将字符串S首尾相连，再选一个位置切开，顺时针或逆时针能够得到字符串S'
+//    容易得到，若S和S'为双生词，则s'和S也为双生词。给定一批仅由英文字母组成的字符串，询问他们之中是否存在双生词。
 
+//    首先给出测试组数t，表示总共有多少数据。
+//    对于每组数据，第一行为一个整数n，表示一共有多少个字符串，接下来的n行，每一行表示一个字符串
+
+//    若存在双生词则输出：Yeah，否则输出Sad
+
+
+
+//思路解析：S和S’字符串，将S'字符串复制一个加上变成S'S'，然后拿S和S'S'进行字符串匹配，或者拿S和（S'的逆序）'S'S进行字符串匹配
+////helloworld
+////hdlrowollehdlrowolle  或者elloworldhelloworldh进行匹配。
+
+
+
+
+//测试结果：
 //3
 //2
 //helloworld
@@ -23,6 +41,17 @@ public class num3 {
 //abcde
 //acbde
 //Sad
+
+//2
+//2
+//ggsd
+//sfgfdgg
+//Sad
+//3
+//asdfg
+//dfgas
+//gfdsa
+//Yeah
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
