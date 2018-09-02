944ce698bd5b462862312e824fdb24a908fdd640
HuaLiLiZhang <1151874043@qq.com>
2018-09-02 16:01:21 +0800

    合法的标识符:数组0-9组成的字符串。（可以包含多个前导0），1.若X为合法的标识符，则X为合法的表达式 2.若X为合法的标识符，则（X）为合法的表达式 3.若X和Y均为合法的表达式，则X+Y，X-Y均为合法的表达式 1,100,1+1,(10) 1-(3-2)

---
+++ src/InterviewDirectory/toutiao2019_2/getTheEquationSolution.java
@@ -0,0 +1,13 @@
+package InterviewDirectory.toutiao2019_2;
+
+import java.util.HashMap;
+
+/**
+ * Created by huali on 2018/8/25.
+ */
+public class num5 {
+    public static void main(String[] args)
+    {
+        HashMap<String,Integer> map = new HashMap<>(15);
+    }
+}
