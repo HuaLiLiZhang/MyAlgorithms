ee3934b5c98acd226ffdbcafed560e70021002c0
HuaLiLiZhang <1151874043@qq.com>
2018-09-20 21:04:29 +0800

    每行输入一个字符串，第一个数为多少进制，中间用#分开，第二个数为进制下的数值。输出，只出现一次的数

---
+++ src/InterviewDirectory/xiaomi2019/Main11.java
@@ -0,0 +1,7 @@
+package InterviewDirectory.xiaomi2019;
+
+/**
+ * Created by huali on 2018/9/20.
+ */
+public class Main11 {
+}
---
+++ src/InterviewDirectory/xiaomi2019/onlydisplayOnesjinzhi.java
@@ -0,0 +1,93 @@
+package InterviewDirectory.xiaomi2019;
+
+import java.util.*;
+
+/**
+ * Created by huali on 2018/9/20.
+ */
+public class onlydisplayOnesjinzhi {
+//    每行输入一个字符串，第一个数为多少进制，中间用#分开，第二个数为进制下的数值。输出，只出现一次的数
+// 输出，只有只出现一次的数。就是这个数值与其他数值都不一样就输出。只出现一次的数。如果都不一样，或者很多不一样，都要输出。
+//10#15
+//4#32
+//4#33
+//8#17
+//11#12
+//12#16
+//14#11
+//END
+//4#32
+//11#12
+//12#16
+
+//10#15
+//4#32
+//4#33
+//8#17
+//END
+//4#32
+
+
+
+//10#15
+//14#11
+//4#33
+//8#17
+//END
+//None
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        Map<Integer,List<Integer>> map = new LinkedHashMap<>();
+        int sum = 0;
+        while (sc.hasNext())
+        {
+            String temp = sc.nextLine();
+            if(temp.equals("END"))
+                break;
+            else
+            {
+                list.add(temp);
+                String[] str = temp.split("#");
+                int jinzhi = Integer.parseInt(str[0]);
+                int m = Integer.parseInt(str[1]);
+                int N = 0,count=0;
+                while (m!=0)
+                {
+                    N += (m%10)*Math.pow(jinzhi,count);
+                    count++;
+                    m = m/10;
+                }
+
+
+                if(map.containsKey(N))
+                {
+                    List<Integer> li = map.get(N);
+                    li.add(sum);
+                    map.put(N,li);
+                }
+                else
+                {
+                    List<Integer> li = new ArrayList<>();
+                    li.add(sum);
+                    map.put(N,li);
+                }
+                sum++;
+
+
+            }
+        }
+        int count = 0;
+        for(Map.Entry<Integer, List<Integer>> entry:map.entrySet())
+        {
+            if(entry.getValue().size()==1) {
+                System.out.println(list.get(entry.getValue().get(0)));
+                count++;
+            }
+        }
+        if(count==0)
+            System.out.println("None");
+    }
+}
