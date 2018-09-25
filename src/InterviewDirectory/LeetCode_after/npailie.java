7580635b228f7fa3c91ee75e47f3ab41a110c15b
HuaLiLiZhang <1151874043@qq.com>
2018-09-25 18:59:36 +0800

    输出前n个自然数的从小到大的所有排列

--- src/InterviewDirectory/LeetCode_after/npailie.java
+++ src/InterviewDirectory/LeetCode_after/npailie.java
@@ -1,7 +1,55 @@
 package InterviewDirectory.LeetCode_after;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by huali on 2018/9/25.
  */
 public class npailie {
+    //3
+    //123
+    //132
+    //213
+    //231
+    //312
+    //321
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<String> list = new ArrayList<>();
+        String s = "";
+        for(int i = 0;i<n;i++)
+        {
+            s +=String.valueOf(i+1);
+        }
+        backtrackPermute(list, new ArrayList<>() , s);
+        for(int i = 0;i<list.size();i++)
+        {
+            System.out.println(list.get(i));
+        }
+    }
+
+    private static void backtrackPermute(List<String> list, List temp,
+                                         String nums) {
+        if (temp.size() == nums.length()) {
+            String t0 = new String();
+            for(Object ch : temp)
+            {
+                t0 += ch;
+            }
+            list.add(t0);
+        } else{
+            for(int i=0;i<nums.length();i++)
+            {
+                if(temp.contains(nums.charAt(i)))//元素已经存在。
+                    continue;
+                temp.add(nums.charAt(i));
+                backtrackPermute(list, temp, nums);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
 }
